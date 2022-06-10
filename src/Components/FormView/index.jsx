0fa81edc9a561c3809e4c2472cc3438a8cf73d75
RoshniDhand1 <roshni.weproinc@gmail.com>
2022-06-10 13:25:27 +0530

    FormView

--- src/Components/FormView/index.jsx
+++ src/Components/FormView/index.jsx
@@ -1,7 +1,7 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Box, FormLabel, TextField,
-    Grid, FormHelperText,TextareaAutosize,
+    Grid, FormHelperText, TextareaAutosize,
     Button, Typography, FormControl,
     RadioGroup, FormControlLabel, Radio, Checkbox
 } from '@mui/material';
@@ -17,94 +17,100 @@ const states = [
     'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
 ]
 
-const discuss_items = [{ name : "TSP (Thrift Savings Plan)" },
-    {name:"SSB (Surviving Spousal Benefit)"},
-    {name:"FEGLI (Life Insurance)"},
-    {name:"Social Security Estimate"},
-    {name: "FERS Supplement"},
-    {name:"Generate Tax-Free Income"},
-    {name: "FERS/CSRS Pension"},
-    {name : "input"}
+const discuss_items = [{ name: "TSP (Thrift Savings Plan)" },
+{ name: "SSB (Surviving Spousal Benefit)" },
+{ name: "FEGLI (Life Insurance)" },
+{ name: "Social Security Estimate" },
+{ name: "FERS Supplement" },
+{ name: "Generate Tax-Free Income" },
+{ name: "FERS/CSRS Pension" },
+{ name: "input" }
 ];
 
 
 const useStyles = makeStyles({
     button: {
-      width:"9.6%",
-      backgroundColor: '#3c52b2',
-      color: '#fff',
-      '&:hover': {
-        backgroundColor: '#1ca2ff !important',
-         "&.active": {
-      backgroundColor:'black',
+        width: "9.6%",
+        backgroundColor: '#3c52b2',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: '#1ca2ff !important',
+            "&.active": {
+                backgroundColor: 'black',
+            },
+        },
     },
-    },
-  },
- activeButton:{
-     backgroundColor:"#1ca2ff !important"
- }
+    activeButton: {
+        backgroundColor: "#1ca2ff !important"
+    }
 })
 
-export default function FormView() {
-    const [user,setUser] = useState([]);
-    const [select,setSelect]= useState();
+export default function FormView({ width, isMobile ,isTablet}) {
+    const [user, setUser] = useState([]);
+    const [select, setSelect] = useState();
     const [ageError, setAgeError] = useState(false);
     const [stateError, setStateError] = useState(false);
     const [value, setValue] = React.useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         setUser(discuss_items);
-    },[]);
+    }, []);
 
     const classes = useStyles();
- 
-    const handleRadioChange = (e)=>{
+
+    const handleRadioChange = (e) => {
         setValue(e.target.value);
         setAgeError('');
-       
+
     }
 
-   const handleChange =(e)=>{
-       const{ name , checked } = e.target;
-       let tempUser = user.map((user)=>user.name === name  ?{...user,isChecked:checked}:user);
-       setUser(tempUser);
-   }
+    const handleChange = (e) => {
+        const { name, checked } = e.target;
+        let tempUser = user.map((user) => user.name === name ? { ...user, isChecked: checked } : user);
+        setUser(tempUser);
+    }
 
-   const handleState =(e)=>{
-       console.log("e::",e.target.value)
-       setSelect(e.target.value)
-   }
+    const handleState = (e) => {
+        console.log("e::", e.target.value)
+        setSelect(e.target.value)
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
-    if (value === '21 to 44') {
-        setAgeError('')
-      } else if (value === '45 to 58') {
-       setAgeError('')
-       } else if(value === '59+'){
-        setAgeError('')
-        
-      } else {
-        setAgeError('Please select an option.');
-      }
+        if (value === '21 to 44') {
+            setAgeError('')
+        } else if (value === '45 to 58') {
+            setAgeError('')
+        } else if (value === '59+') {
+            setAgeError('')
 
-      if(!select){
-          setStateError("Please Select Atleast One State")
-      }
-      
+        } else {
+            setAgeError('Please select an option.');
+        }
+
+        if (!select) {
+            setStateError("Please Select Atleast One State")
+        }
+
     }
     return (
         <Box>
-            <Typography fontWeight="bold" sx={{ color: '#595959', fontStyle: 'normal' }}>
+            <Typography fontWeight="bold" sx={{ color: '#595959', fontSize: '18px' }}>
                 Enter Details
             </Typography>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <TextField fullWidth label="Name *" variant="outlined" />
+            <Grid container spacing={4} sx={{ py: 4 }}>
+                <Grid item xs={12} md={6}>
+                    <TextField
+                        label='Name*' fullWidth
+                        variant="outlined"
+                    />
                 </Grid>
-                <Grid item xs={6}>
-                    <TextField fullWidth label="Email *" variant="outlined" />
+                <Grid item xs={12} md={6}>
+                    <TextField
+                        label='Email*' fullWidth
+                        variant="outlined"
+                    />
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                     <TextField
                         fullWidth
                         variant="outlined"
@@ -116,25 +122,35 @@ export default function FormView() {
                         }}
                     />
                 </Grid>
-                <Grid item >
+
+
+                <Grid item>
                     <FormControl>
-                        <FormLabel>Age Range*</FormLabel>
+                        <FormLabel style={{ fontWeight: 'bold', color: 'black' }}>Age Range*</FormLabel>
                         <RadioGroup
                             row
                             onChange={handleRadioChange}
                             helperText={ageError}
                             error={ageError}
+                            style={{ flexWrap: 'nowrap' }}
+
                         >
+
                             <FormControlLabel value="21 to 44" control={<Radio />} label="21 to 44" />
                             <FormControlLabel value="45 to 58" control={<Radio />} label="45 to 58" />
                             <FormControlLabel value="59+" control={<Radio />} label="59+" />
                         </RadioGroup>
-                        <FormHelperText error ={true}>{ageError}</FormHelperText>
+                        <FormHelperText error={true}>{ageError}</FormHelperText>
                     </FormControl>
                 </Grid>
-                <Grid item xs={12}>
-                    <FormLabel>State * 
-                  {select?"":  <FormHelperText error ={true}>{stateError}</FormHelperText>}
+
+
+
+            </Grid>
+            <Grid container spacing={12}>
+                <Grid item xs={24} md={12}>
+                    <FormLabel style={{ fontWeight: 'bold', color: 'black' }}>State *
+                        {select ? "" : <FormHelperText error={true}>{stateError}</FormHelperText>}
 
                     </FormLabel>
                     <Grid container
@@ -146,10 +162,11 @@ export default function FormView() {
                                 return (
                                     <Grid item>
 
-                                        <Button variant="outlined" 
-                                        className={select === state ? classes.activeButton:classes.button} 
-                                        value ={state} 
-                                        outlinedButton onClick ={handleState}>
+                                        <Button variant="outlined"
+                                            style={{ display: 'flex', direction: 'row' }}
+                                            className={select === state ? classes.activeButton : classes.button}
+                                            value={state}
+                                            outlinedButton onClick={handleState}>
                                             {state}
                                         </Button>
                                     </Grid>
@@ -158,45 +175,56 @@ export default function FormView() {
                         }
                     </Grid>
                 </Grid>
-                
-                <Grid item xs={12}>
-                    <FormLabel> Please check off any of the following item(s) you'd like to discuss:</FormLabel>
+            </Grid>
+
+            <Grid container spacing={2} sx={{ py: 4 }}>
+                <Grid item xs={24} md={12}>
+                    <FormLabel style={{ fontWeight: 'bold', color: 'black' }}> Please check off any of the following item(s) you'd like to discuss:</FormLabel>
                     <Grid container>
                         {
                             user.map((item) => (
-                              <Grid item xs={6}>
-                                <FormControlLabel
-                                    control={
-                                    <Checkbox
-                                       checked ={item?.isChecked || false}
-                                       onChange={handleChange}
-                                        name={item.name}
-                                        color="primary"
+                                <Grid item  xs={isMobile|| isTablet?'':6}>
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox
+                                                checked={item?.isChecked || false}
+                                                onChange={handleChange}
+                                                name={item.name}
+                                                color="primary"
+                                            />
+                                        }
+                                        label={item.name === "input" ? "" : item.name}
+
                                     />
-                                    }
-                                    label={item.name ==="input"?"":item.name}
-                                
-                                />
-                               {item.name === "input" ? <TextField  disabled={item?.isChecked? false : true}></TextField> : ""}
+                                    {item.name === "input" ?
+                                        <TextField
+                                            disabled={item?.isChecked ? false : true}
+                                    
+                                        >
+
+                                        </TextField>
+                                        :
+                                        ""}
                                 </Grid>
                             ))
                         }
 
                     </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <FormLabel>Please share anything that will help prepare for our meeting:</FormLabel>
+
+                <Grid item xs={12} >
+                    <FormLabel style={{ fontWeight: 'bold', color: 'black' }}>Please share anything that will help prepare for our meeting:</FormLabel>
                     <TextareaAutosize
-                    aria-label="minimum height"
-                    placeholder=""
-                    style={{ width: '435px', height:'101px' }}
+                        aria-label="minimum height"
+                        placeholder=""
+                        style={{ width: '100%', height: '101px' }}
                     />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} md={6}>
                     <Button variant="contained" onClick={handleSubmit}>Schedule Event</Button>
                 </Grid>
-
             </Grid>
+
         </Box>
 
     )
--- src/Containers/index.jsx
+++ src/Containers/index.jsx
@@ -1,51 +1,100 @@
-import React,{useState} from 'react'
-import { Paper, Box} from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { Paper, Box } from '@mui/material'
 import Calender from '../Components/Calender';
 import { StyleSelectBox } from '../Themes';
 import AppointmentDetail from '../Components/AppointmentDetail';
 import TimerView from '../Components/TimerView';
 import FormView from '../Components/FormView';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import SelectTimeZone from '../Components/SelectTimeZone';
 
 export default function Containers() {
+    const hasWindow = typeof window !== 'undefined';
+    const [isDesktop, setIsDesktop] = useState(getWindowDimensions().width > 1100);
+    const [isTablet, setIsTablet] = useState(getWindowDimensions().width >= 600 && getWindowDimensions().width <= 1100);
+    const [isMobile, setIsMobile] = useState(getWindowDimensions().width < 600);
+
+    console.log("screen:: D-", isDesktop, " M-", isMobile, " T-", isTablet);
+
+    useEffect(() => {
+        if (hasWindow) {
+            function handleResize() {
+                setIsDesktop(getWindowDimensions().width > 1100);
+                setIsTablet(getWindowDimensions().width >= 600 && getWindowDimensions().width <= 1100);
+                setIsMobile(getWindowDimensions().width < 600);
+            }
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow]);
+
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height,
+        };
+    }
+
     //TODO: state for is date selected
-     const [onDateSelect,setOnDateSelect] = useState(null);
-     const matches = useMediaQuery('(min-width:300px)');
+    const [onDateSelect, setOnDateSelect] = useState(null);
     const classes = StyleSelectBox();
     return (
-            // desktop view when no TimerView 
-<>
+        // desktop view when no TimerView 
+        <>
+            <Paper sx={
+                isDesktop ?
+                    {
+                        width: onDateSelect == null ? "50%" : "65%",
+                        display: "flex", justifyContent: "space-between", alignItem: "center",
+                        my: 5, mx: 'auto',
+                        border: '1px solid #d3d3d3',
+                    }
+                    : { width: "90%", alignItem: "center", mt: 5, mx: 'auto' }
+            }>
 
-        
-            <Paper sx={{ width:onDateSelect == null ?"70%":"90", display:"flex", justifyContent:"center", alignItem:"center", mt:5, mx:'auto'}}>
-            
-             {/* desktop view when TimerView  */}
-             {/* <Paper sx={{ width:"90%", alignItem:"center", mt:5, mx:'auto'}}> */}
-             {/* <Paper sx={{ width:"90%", display:"flex", justifyContent:"center", alignItem:"center", mt:5, mx:'auto'}}> */}
                 <Box>
                     <AppointmentDetail />
                 </Box>
                 {/* Mobile View */}
-                
+
                 {/* <Box width={"100%"} sx={{ borderTop: '0.1px solid #cdcdcd' }}> */}
-                <Box width={"100%"} sx={matches?{ borderTop: '0.1px solid #cdcdcd' }:{ borderLeft: '0.1px solid #cdcdcd' }}>
+                <Box width={ isDesktop ?  "65%" :"100%"} sx={ isDesktop ? { borderLeft: '0.1px solid #cdcdcd' } : { borderTop: '0.1px solid #cdcdcd' } }>
                     <Box sx={{ p: 5 }}>
-                        <Box sx={{display:"flex", justifyContent:"space-between", alignItem:"center"}}>
-                        <FormView/>
-                            {/* <Calender onDateSelect ={onDateSelect} setOnDateSelect={setOnDateSelect}/> */}
-                            {/* Todo: If date selected then show the timer */}
-                           {/* {onDateSelect &&                                                                      
-                            <TimerView width="40%"/>
-                           } */}
+                        <Box sx={{ display: "flex", justifyContent: "space-between", alignItem: "center" }}>
+                            {/* <FormView /> */}
+                            
+                             {/* {(isDesktop || isTablet || (isMobile && onDateSelect == null)) && <Box>
+                                        <Calender 
+                                           onDateSelect ={onDateSelect} 
+                                           setOnDateSelect={setOnDateSelect}
+                                            isMobile ={isMobile}
+                                            
+                                        />
+                                        
+                                    </Box>}
+                                    {onDateSelect && 
+                                    <Box>
+                                    <SelectTimeZone 
+                                        isMobile ={isMobile}
+                                    />
+                                    <TimerView 
+                                       width={ (!isMobile && onDateSelect !== null) ? "40%" : "90%"} 
+                                        isMobile={isMobile} />
+                                    </Box>} */}
+                                    <FormView
+                                    isMobile ={isMobile}
+                                    isTablet={isTablet}
+                                    />
                         </Box>
                     </Box>
                 </Box>
-              
+
             </Paper>
-          
-        
-          
-</>
-            
+
+
+
+        </>
+
     )
 }
